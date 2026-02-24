2d1c5ac72187c4c3bc01c608dfc35256f3d2048c
Ana1M1 <mihailovana72@gmail.com>
2026-02-24 13:00:00 +0200

    wip

--- content.js
+++ content.js
@@ -24,11 +24,33 @@ const data = {
             Text: "Luci și hidratare",
             Pret: "Preț: 220 MDL",
         },
-        "product_images/hair_oil.png",
-        "product_images/honey_hair_oil.webp",
-        "product_images/el_hair_oil.jpg",
-        "product_images/el_shampoo.webp",
-        "product_images/el_hair_mask.avif"
+        {   Link: "product_images/hair_oil.png",
+            Nume: "Hair Oil",
+            Text: "Hidratare Îndelungată",
+            Pret: "Preț: 220 MDL",
+
+        },
+        {   Link:"product_images/honey_hair_oil.webp",
+            Nume: "Hair Oil",
+            Text: "Hrană pentru fire",
+            Pret: "Preț: 350 MDL",
+
+        },
+        {   Link:"product_images/el_hair_oil.jpg",
+            Nume: "Hair Oil",
+            Text: "Spune nu firelor despicate",
+            Pret: "Preț: 399 MDL",
+        },
+        {   Link:"product_images/el_shampoo.webp",
+            Nume: "Elvive- Shampon",
+            Text: "Cu efect garantat",
+            Pret: "Preț: 450 MDL",
+        },
+        {   Link:"product_images/el_hair_mask.avif",
+            Nume: "HAIR MASK",
+            Text: "Claitate cu efect garantat",
+            Pret: "Preț: 350 MDL",
+        },
     ]
 };
 console.log("data: ", data);
@@ -48,15 +70,17 @@ const hero = document.getElementById("background")
 console.log("hero: ", hero);
 hero.setAttribute("src", data.HeroImage);
 
-const productCards = document.getElementsByClassName("product-card");
-console.log(productCards);
-for (let i = 0; i < productCards.length; i++) {
-    console.log(productCards[i]);
-    const imageLink = data.ImaginiLinks[i];
-    const image = document.createElement("img");
-    image.setAttribute("src", imageLink);
-    productCards[i].prepend(image)
-}
+
+
+// const productCards = document.getElementsByClassName("product-card");
+// console.log(productCards);
+// for (let i = 0; i < productCards.length; i++) {
+//     console.log(productCards[i]);
+//     const imageLink = data.ImaginiLinks[i];
+//     const image = document.createElement("img");
+//     Link.Nume.Text.Pret.setAttribute("src", imageLink);
+//     productCards[i].prepend(image)
+// }
 
 
 
@@ -80,3 +104,19 @@ for (let i = 0; i < productCards.length; i++) {
 
 //     currentChild.age;
 // }
+const productCards = document.getElementsByClassName("product-card");
+console.log(productCards);
+for (let i = 0; i < productCards.length; i++) {
+    console.log(productCards[i]);
+    const product = data.Produse[i];
+    const image = document.createElement("img");
+    image.setAttribute("src", product.Link);
+    productCards[i].appendChild(image);
+
+    // create h3 and set innerText tp product.Name
+    // 
+     const name = document.createElement("h3");
+    name.innerText = product.Nume;
+    productCards[i].appendChild(name);
+
+}
