ad7aef32ded3c0dc5470268908c85d9e938a20a8
Ana1M1 <mihailovana72@gmail.com>
2026-03-10 12:43:21 +0200

    sugestile se salveaza in local storage

--- script.js
+++ script.js
@@ -1,69 +1,97 @@
 console.log("hello");
 
 const consoleBtn = document.getElementById("button");
+
 const consoleBtnClick = () => {
   console.log("console", Math.random());
-}
+};
 
 consoleBtn.addEventListener("click", consoleBtnClick);
 
+console.log("Hello from console")
 
-
-
-console.log("Hello from console");
 
 const lightBg = "rgb(236, 190, 197)";
-
 const body = document.getElementsByTagName("body")[0];
 const themeButton = document.getElementById("theme-button");
 
-themeButton.addEventListener("click", function (){
+themeButton.addEventListener("click", function () {
   const currentBodyBg = window.getComputedStyle(body).background;
 
   if (currentBodyBg.includes(lightBg)) {
     body.style.background = "rgb(57, 53, 80)";
     body.style.color = "#FFF";
-
-    themeButton.innerText = "☀️"
+    themeButton.innerText = "☀️";
   } else {
     body.style.background = lightBg;
     body.style.color = "#000";
-
-    themeButton.innerText = "🌑"
+    themeButton.innerText = "🌑";
   }
-})
+});
+
+
+let products = JSON.parse(localStorage.getItem("products")) || [];
+
+function createProductCard(product) {
+
+  const productContainer = document.querySelector(".product");
+
+  const card = document.createElement("div");
+  card.classList.add("product-card");
+
+  const img = document.createElement("img");
+  img.src = product.image;
+
+  const title = document.createElement("h3");
+  title.innerText = product.name;
+
+  const desc = document.createElement("p");
+  desc.innerText = product.text;
+
+  const pret = document.createElement("p");
+  pret.classList.add("price");
+  pret.innerText = product.price;
+
+  card.appendChild(img);
+  card.appendChild(title);
+  card.appendChild(desc);
+  card.appendChild(pret);
+
+  productContainer.appendChild(card);
+}
+
+
+products.forEach(product => {
+  createProductCard(product);
+});
+
+
+
 const addButton = document.getElementById("add-product");
 
-addButton.addEventListener("click", function(){
+addButton.addEventListener("click", function () {
 
-const name = document.getElementById("product-name").value;
-const image = document.getElementById("product-image").value;
-const text = document.getElementById("product-text").value;
-const price = document.getElementById("product-price").value;
+  const name = document.getElementById("product-name").value;
+  const image = document.getElementById("product-image").value;
+  const text = document.getElementById("product-text").value;
+  const price = document.getElementById("product-price").value;
 
-const productContainer = document.querySelector(".product");
+  const newProduct = {
+    name: name,
+    image: image,
+    text: text,
+    price: price
+  };
 
-const card = document.createElement("div");
-card.classList.add("product-card");
+  products.push(newProduct);
 
-const img = document.createElement("img");
-img.src = image;
+  localStorage.setItem("products", JSON.stringify(products));
 
-const title = document.createElement("h3");
-title.innerText = name;
+  createProductCard(newProduct);
 
-const desc = document.createElement("p");
-desc.innerText = text;
-
-const pret = document.createElement("p");
-pret.classList.add("price");
-pret.innerText = price;
-
-card.appendChild(img);
-card.appendChild(title);
-card.appendChild(desc);
-card.appendChild(pret);
-
-productContainer.appendChild(card);
+  document.getElementById("product-name").value = "";
+  document.getElementById("product-image").value = "";
+  document.getElementById("product-text").value = "";
+  document.getElementById("product-price").value = "";
 
 });
